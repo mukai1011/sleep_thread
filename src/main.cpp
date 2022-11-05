c7b45f3a7b9a49f467f73220ac6901095378c552
Koutaro Mukai <mukai.k1011k@outlook.jp>
2022-11-05 12:29:25 +0900

    Add test case

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,61 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/chrono.h>
+
+#include <atomic>
+#include <chrono>
+#include <thread>
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-int add(int i, int j) {
-    return i + j;
+std::thread th_;
+
+class Event
+{
+    std::atomic<bool> flag_ = false;
+
+public:
+    void Set() { flag_ = true; }
+    bool IsSet() { return flag_; }
+};
+
+void start(std::chrono::duration<long long> duration, Event &event) {
+    auto busy_wait = [](std::chrono::duration<long long> duration, Event &event)
+    {
+        auto current_time = std::chrono::system_clock::now();
+        while (std::chrono::system_clock::now() < current_time + duration && !event.IsSet())
+            ;
+    };
+    th_ = std::thread(busy_wait, duration, std::ref(event));
 }
+
+void join() {
+    th_.join();
+}
+
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(python_example, m) {
-    m.doc() = R"pbdoc(
-        Pybind11 example plugin
-        -----------------------
+PYBIND11_MODULE(python_example, m)
+{
+    py::class_<Event>(m, "Event")
+        .def(py::init())
+        .def("set", &Event::Set)
+        .def("is_set", &Event::IsSet);
 
-        .. currentmodule:: python_example
+    m.def("start", [](std::chrono::duration<long long> duration, Event &cancel) {
+        py::gil_scoped_release _;
+        start(duration, cancel);
+    }, R"pbdoc(
+        Subtract two numbers
 
-        .. autosummary::
-           :toctree: _generate
-
-           add
-           subtract
-    )pbdoc";
-
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
-
-        Some other explanation about the add function.
+        Some other explanation about the subtract function.
     )pbdoc");
 
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+    m.def("join", []() {
+        py::gil_scoped_acquire _;
+        join();
+    }, R"pbdoc(
         Subtract two numbers
 
         Some other explanation about the subtract function.
--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,79 @@
+from datetime import timedelta
+import threading
+import multiprocessing
+from time import perf_counter
+
 import python_example as m
 
-assert m.__version__ == '0.0.1'
-assert m.add(1, 2) == 3
-assert m.subtract(1, 2) == -1
+def sleep(duration: timedelta, event):
+    total_seconds = duration.total_seconds()
+    current_time = perf_counter()
+    while perf_counter() < current_time + total_seconds and not event.is_set():
+        pass
+
+def sub_thread(duration: timedelta, event):
+
+    print()
+
+    # 1. Threadのビジーウェイト
+    print("--- Thread ---")
+    th_event = threading.Event()
+    th = threading.Thread(target=sleep, args=(duration, th_event))
+    
+    current_time = perf_counter()
+    th.start()
+    th.join()
+    print(perf_counter() - current_time)
+
+    # 2. Processのビジーウェイト
+    print("--- Process ---")
+    proc_event = multiprocessing.Event()
+    proc = multiprocessing.Process(target=sleep, args=(duration, proc_event))
+    
+    current_time = perf_counter()
+    proc.start()
+    proc.join()
+    print(perf_counter() - current_time)
+    
+    # 3. m.start() ~ m.join()のビジーウェイト
+    print("--- C++ ---")
+    m_event = m.Event()
+    
+    current_time = perf_counter()
+    m.start(duration, m_event)
+    m.join()
+    print(perf_counter() - current_time)
+    
+    print()
+    event.set()
+
+def fibonacci(n, event):
+    if event.is_set():
+        raise Exception()
+
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n - 1, event) + fibonacci(n - 2, event)
+
+def main():
+
+    duration = timedelta(seconds=10)
+    event = threading.Event()
+    
+    sub_th = threading.Thread(target=sub_thread, args=(duration, event))
+    sub_th.start()
+
+    # Main thread is supercalifragilisticexpialidocious-ly busy
+    try:
+        while not event.is_set():
+            fibonacci(100, event)
+    except:
+        pass
+    
+    sub_th.join()
+
+if __name__ == "__main__":
+    main()
